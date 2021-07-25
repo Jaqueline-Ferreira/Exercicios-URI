a8377bf781ce0e6725099fc32caa79abf2545de8
Jaqueline26 <ferreirasousajaqueline@gmail.com>
2021-07-25 20:36:06 -0300

    Exercicios URI

---
+++ Tabuada/src/Main/principal.java
@@ -0,0 +1,34 @@
+package Main;
+
+public class principal {
+	public static void main(String[] args) {
+		int numeroBase = 13;
+		
+		int a = 1,
+	        a1 = 2,
+	        a2 = 3,
+	        a3 = 4,
+	        a4 = 5,
+	        a5 = 6,
+	        a6 = 7,
+	        a7 = 8,
+	        a8 = 9,
+	        a9 = 10;
+	        
+	       System.out.println(a + "x" + numeroBase +"=" + numeroBase * a);
+		   System.out.println(a1 + "x" + numeroBase +"=" + numeroBase * a1);
+		   System.out.println(a2 + "x" + numeroBase +"=" + numeroBase * a2);
+	       System.out.println(a3 + "x" + numeroBase +"=" + numeroBase * a3);
+	       System.out.println(a4 + "x" + numeroBase +"=" + numeroBase * a4);
+	       System.out.println(a5 + "x" + numeroBase +"=" + numeroBase * a5);
+	       System.out.println(a6 + "x" + numeroBase +"=" + numeroBase * a6);
+	       System.out.println(a7 + "x" + numeroBase +"=" + numeroBase * a7);
+	       System.out.println(a8 + "x" + numeroBase +"=" + numeroBase * a8);
+	       System.out.println(a9 + "x" + numeroBase +"=" + numeroBase * a9);
+	       
+	       
+	       
+	       
+	}
+}
+	
