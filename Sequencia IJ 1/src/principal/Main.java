09bcb19056d15d57704a1600ca36dfc1d593c9fc
Jaqueline26 <ferreirasousajaqueline@gmail.com>
2021-07-25 21:18:46 -0300

    Exercio de sequencia

---
+++ Sequencia IJ 1/src/principal/Main.java
@@ -0,0 +1,19 @@
+package principal;
+
+public class Main {
+	
+	public static void main (String[] args) {
+		 
+		int i = 1, j = 60;
+		System.out.println( "I=" + i + " J=" + j) ;
+		for (int x = 0; x < 12; x++) {
+			i = i + 3;
+			j = j - 5;
+			System.out.println( "I=" + i + " J=" + j) ;
+			
+		}
+				
+	}
+	
+
+}
