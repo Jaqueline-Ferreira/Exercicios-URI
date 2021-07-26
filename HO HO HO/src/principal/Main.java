cc220e37a3865a73635b3e8124510e7e111a2b61
Jaqueline26 <ferreirasousajaqueline@gmail.com>
2021-07-25 23:40:47 -0300

    Exercicios URI

---
+++ HO HO HO/src/principal/Main.java
@@ -0,0 +1,21 @@
+package principal;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		int quantidade = 10;
+		
+		String ho = "";
+		
+		for (int i = 0; i < quantidade - 1; i++) {
+			ho = ho + "Ho ";
+		}
+		
+		ho = ho + "Ho!";
+		
+		System.out.println(ho);
+		
+	}		
+
+}
